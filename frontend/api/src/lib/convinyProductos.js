4dab1c4bd0c46641117565b9f5222871f2b6f20d
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-11-21 11:36:53 -0600

    DB: add batch of conviny products

---
+++ frontend/api/src/lib/convinyProductos.js
@@ -0,0 +1,12 @@
+const convinyProductos = [
+  ['manzana', 3.33, 'k', 'https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['pera', 3.14, 'k', 'https://images.pexels.com/photos/7214835/pexels-photo-7214835.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['agua embotellada', .24, 'l', 'https://images.pexels.com/photos/2479095/pexels-photo-2479095.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['refresco', .82, 'l', 'https://images.pexels.com/photos/2121746/pexels-photo-2121746.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['jugo', 1.22, 'l', 'https://images.pexels.com/photos/161440/smoothie-fruit-vegetables-salad-beetroot-carrots-161440.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['cafe', 30.18, 'k', 'https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['té', 110.73, 'k', 'https://images.pexels.com/photos/5946762/pexels-photo-5946762.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['atun', 6.15, 'k', 'https://images.pexels.com/photos/12001951/pexels-photo-12001951.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['frijoles', 2.03, 'k', 'https://images.pexels.com/photos/9902268/pexels-photo-9902268.jpeg?auto=compress&cs=tinysrgb&w=600'],
+  ['sardina', 4.5, 'k', 'https://images.pexels.com/photos/9902268/pexels-photo-9902268.jpeg?auto=compress&cs=tinysrgb&w=600'],
+]
