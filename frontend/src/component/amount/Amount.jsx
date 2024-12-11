8bb0777cde0ff2205921c34d08ecd84971769377
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-12-11 04:02:01 -0600

    Feat: add logo

--- frontend/src/component/amount/Amount.jsx
+++ frontend/src/component/amount/Amount.jsx
@@ -2,13 +2,14 @@ import { Form } from 'react-router-dom'
 import './Amount.css'
 import InputNum from '../InputNum/InputNum'
 import Btn from '../Button/Btn'
+import logo from '/logo.svg'
 
 export default function Amount() {
   return (
     <>
       <section className="amount">
         <h1>Ahorra como un Campeón</h1>
-        <div className="img"></div>
+        <img src={logo} alt="logo" />
         <p>Por favor ingresa el monto del que te guiaras para que podamos ayudarte a seleccionar las mejores promociones y descuentos.</p>
         <Form>
           <InputNum name='amount' symbol='€' />
