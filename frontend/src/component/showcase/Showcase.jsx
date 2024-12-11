ffaac0f3eb6632179b96363a985f564d10f677f7
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-12-11 00:39:22 -0600

    Refactor: adapt component Showcase to the matrix

--- frontend/src/component/showcase/Showcase.jsx
+++ frontend/src/component/showcase/Showcase.jsx
@@ -10,12 +10,12 @@ export default function Showcase({
       <section className="showcase">
         <h2>{title}</h2>
         <ul>
-          {articles.map(({display_name, thumbnail, brand, slug, price_instructions: { unit_price }, details: { description }}) => <li>
-            <img src={thumbnail} alt={slug} />
-            <h3>{display_name.toLowerCase()}</h3>
-            <p className='description'>{description}</p>
+          {articles.map(([name, image, alt, description, price, brand = 'Hacendado']) => <li>
+            <img src={image} alt={alt} />
+            <h3>{name.toLowerCase()}</h3>
+            <p className='description'>{description}.</p>
             <p className='brand'>{brand}</p>
-            <p className='price'>&euro;{unit_price}<span className='unit'> por unidad</span></p>
+            <p className='price'>&euro;{price.toFixed(2)}<span className='unit'> por unidad</span></p>
             <Btn type='button' >Agregar</Btn>
           </li>)}
         </ul>
