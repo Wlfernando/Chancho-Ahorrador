d12cb18850a2aa471ea953b1365f222bad8dcf46
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-12-11 11:43:34 -0600

    Feat: add section priority

--- frontend/src/component/amount/Amount.jsx
+++ frontend/src/component/amount/Amount.jsx
@@ -3,19 +3,19 @@ import './Amount.css'
 import InputNum from '../InputNum/InputNum'
 import Btn from '../Button/Btn'
 import logo from '/logo.svg'
+import Priority from '../section/Priority'
 
 export default function Amount() {
   return (
     <>
-      <section className="amount">
-        <h1>Ahorra como un Campeón</h1>
+      <Priority title={'Ahorra como un Campeón'} className={'amount'}>
         <img src={logo} alt="logo" />
         <p>Por favor ingresa el monto del que te guiaras para que podamos ayudarte a seleccionar las mejores promociones y descuentos.</p>
         <Form>
           <InputNum name='amount' symbol='€' />
           <Btn mod='forward' />
         </Form>
-      </section>
+      </Priority>
     </>
   )
 }
---
+++ frontend/src/component/section/Priority.jsx
@@ -0,0 +1,16 @@
+import './Priority.css'
+
+export default function Priority({
+  title,
+  className,
+  children,
+}) {
+  return (
+    <>
+      <section className={`priority${className && ' ' + className}`}>
+        <h1>{title}</h1>
+        {children}
+      </section>
+    </>
+  )
+}
