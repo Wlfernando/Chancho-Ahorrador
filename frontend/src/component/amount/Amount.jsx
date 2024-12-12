7065ebe3a4013fc2beba0e34b5e2e16deb4038a0
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-12-12 12:15:41 -0600

    feat: record budget

--- frontend/src/component/amount/Amount.jsx
+++ frontend/src/component/amount/Amount.jsx
@@ -1,20 +1,29 @@
-import { Form } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './Amount.css'
 import InputNum from '../InputNum/InputNum'
 import Btn from '../Button/Btn'
 import logo from '/logo.svg'
 import Priority from '../section/Priority'
+import { useBasketContext } from '../../element/Root'
+import { useState } from 'react'
 
 export default function Amount() {
+  const { budgetRef } = useBasketContext();
+  const [budget, setBudget] = useState(budgetRef.current);
+  const navigate = useNavigate();
+
   return (
     <>
       <Priority title={'Ahorra como un Campeón'} className={'amount'}>
         <img src={logo} alt="logo" />
         <p>Por favor ingresa el monto del que te guiaras para que podamos ayudarte a seleccionar las mejores promociones y descuentos.</p>
-        <Form>
-          <InputNum name='amount' symbol='€' />
-          <Btn mod='forward' />
-        </Form>
+        <form>
+          <InputNum name='budget' symbol='€' onChange={(_, value) => setBudget(value)} value={budget} />
+          <Btn mod='forward' onClick={() => {
+            budgetRef.current = +budget;
+            navigate('../retailer');
+          }} />
+        </form>
       </Priority>
     </>
   )
