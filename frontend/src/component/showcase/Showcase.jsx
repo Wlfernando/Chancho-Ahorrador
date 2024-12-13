e94fb4bc829af552ffed82ecc1e97d99ce9d04be
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-12-12 23:25:28 -0600

    Feat: implement promo in Showcase component

--- frontend/src/component/showcase/Showcase.jsx
+++ frontend/src/component/showcase/Showcase.jsx
@@ -1,26 +1,39 @@
 import { useBasketContext } from '../../element/Root'
 import Btn from '../Button/Btn'
+import IconCurrency from '../icon-currency/IconCurrency';
 import './Showcase.css'
+import iconCompany from '/icon_company.svg'
 
 export default function Showcase({
   title = '',
   articles = [],
+  promo,
 }) {
-  const { recordBasket } = useBasketContext();
+  const { recordBill } = useBasketContext();
 
   return (
     <>
-      <section className="showcase">
+      <section className={`showcase${promo ? ' promo' : ''}`}>
         <h2>{title}</h2>
         <ul>
-          {articles.map(([name, image, alt, description, price, brand = 'Hacendado'], i) => <li key={i}>
-            <img src={image} alt={alt} />
-            <h3>{name.toLowerCase()}</h3>
-            <p className='description'>{description}.</p>
-            <p className='brand'>{brand}</p>
-            <p className='price'>&euro;{price.toFixed(2)}<span className='unit'> por unidad</span></p>
-            <Btn type='button' onClick={() => recordBasket(true, [name, image, alt, description, price, brand])} >Agregar</Btn>
-          </li>)}
+          {articles.map(([name, image, alt, description, price, brand = 'Hacendado', promo], i) => {
+            let isChecked = false;
+
+            return <li key={i}>
+              <img src={image} alt={alt} />
+              <h3>{name.toLowerCase()}</h3>
+              <p className='description'>{description}.</p>
+              <p className='brand'>{brand}</p>
+              <p className='price'><IconCurrency fill='green' width='14' height='12'/>{(price).toFixed(2)}<span className='unit'> por unidad</span></p>
+              {promo &&
+                <label>
+                  <input type="checkbox" onChange={({ target: { checked }}) => isChecked = checked} />
+                  20% en este producto
+                  <img src={iconCompany} alt="icon of your company" />
+                </label>
+              }
+              <Btn type='button' onClick={() => recordBill([name, image, alt, description, price, brand]).set(isChecked ? .2 : undefined)} >Agregar</Btn>
+          </li>})}
         </ul>
       </section>
     </>
