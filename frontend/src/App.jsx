f3ace13c48368955c243187d7c0cabf9ad5a6947
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-12-13 07:54:24 -0600

    feat: add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,57 @@
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom'
 import Root from './element/Root'
+import Shopfront from './element/Shopfront';
+import Amount from './component/amount/Amount';
+import Nav from './component/nav/Nav';
+import Picker from './element/Picker';
+import Offers from './element/Offers';
+import Home from './element/Home';
+import Basket from './element/Basket';
 
-const router = createBrowserRouter([{
+const basketPage = {
+  path: 'basket',
+  element: <Basket />,
+}
+
+const offers = {
+  path: 'offers',
+  element: <Offers />
+}
+
+const retailerPath = {
+  path: 'compilation/:retailer',
+  element: <Picker />,
+};
+
+const budget = {
+  path: 'budget',
+  Component: Amount,
+};
+
+const retailer = {
+  path: 'retailer',
+  Component: Nav,
+}
+
+const board = {
+  path: 'board',
+  element: <Shopfront />,
+  children: [budget, retailer],
+};
+
+const main = {
+  index: true,
+  element: <Home />
+}
+
+const root = {
   path: '/',
-  element: <Root/>,
-}])
+  element: <Root />,
+  children: [main, board, retailerPath, offers, basketPage],
+};
+
+const router = createBrowserRouter([root]);
 
 function App() {
   return <RouterProvider router={router} />
